bee4de518d68540eabc5d514547a03c667b52d26
OKran0706 <93201241+OKran0706@users.noreply.github.com>
2023-02-15 11:30:03 -0500

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Karan has successfully built and run\n";
-  cout << "This is version 2\n"
+  cout << "This is version 2\n";
   return 0;
 }
