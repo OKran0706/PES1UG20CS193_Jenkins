1740b7fa170f3110ad50695a6db8c4c90660eae4
OKran0706 <93201241+OKran0706@users.noreply.github.com>
2023-02-15 12:34:58 -0500

    Create CS193_Karan.cpp

---
+++ CS193_Karan.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins Pipeline\n";
+  cout << "Karan has successfully built and run the Pipeline\n";
+  return 0;
+}
